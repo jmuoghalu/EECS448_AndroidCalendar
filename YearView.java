51a50cda11de1c6cbe3dbefbcd80afc1cdca0d65
fisherc516 <c086f573@ku.edu>
2016-09-16 08:25:58 -0500

    YearView can tell MonthView which month to display

--- YearView.java
+++ YearView.java
@@ -31,8 +31,63 @@ public class YearView extends AppCompatActivity implements View.OnClickListener 
         Intent goToMonth = new Intent(this, MonthView.class);
         switch(view.getId())
         {
-            default:
+            case(R.id.augustButton):
             {
+                goToMonth.putExtra(MONTH, 8);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.septemberButton):
+            {
+                goToMonth.putExtra(MONTH, 9);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.octoberButton):
+            {
+                goToMonth.putExtra(MONTH, 10);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.novemberButton):
+            {
+                goToMonth.putExtra(MONTH, 11);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.decemberButton):
+            {
+                goToMonth.putExtra(MONTH, 12);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.januaryButton):
+            {
+                goToMonth.putExtra(MONTH, 1);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.februaryButton):
+            {
+                goToMonth.putExtra(MONTH, 2);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.marchButton):
+            {
+                goToMonth.putExtra(MONTH, 3);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.aprilButton):
+            {
+                goToMonth.putExtra(MONTH, 4);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.mayButton):
+            {
+                goToMonth.putExtra(MONTH, 5);
                 startActivity(goToMonth);
                 break;
             }
