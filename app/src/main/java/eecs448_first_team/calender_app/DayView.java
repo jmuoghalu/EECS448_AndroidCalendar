4a3361564782d24b9da21e40a1d297ad4eddcc37
DESKTOP-PCV9COG\Hans <hansskywalkerr@gmail.com>
2016-09-17 17:12:32 -0500

    Moved all classes to Java class directory unser src folder, moved all layouts to res/layouts under src folder. Added listener to 6th week of month. Edited Manifest to include (but you can't get to them) each new activity.

---
+++ app/src/main/java/eecs448_first_team/calender_app/DayView.java
@@ -0,0 +1,124 @@
+package eecs448_first_team.calender_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class DayView extends AppCompatActivity implements View.OnClickListener {
+    public final static String DATA = "";
+    public int[] array;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_view);
+
+        findViewById(R.id.weekButton).setOnClickListener(this);
+        findViewById(R.id.monthButton).setOnClickListener(this);
+        findViewById(R.id.yearButton).setOnClickListener(this);
+        findViewById(R.id.addDetailsButton).setOnClickListener(this);
+
+        Intent getToDay = getIntent();
+        array = getToDay.getIntArrayExtra(WeekView.DATA);
+        array = getToDay.getIntArrayExtra(AddDetails.DATA);
+        fillDate();
+    }
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            case(R.id.weekButton):
+            {
+                Intent goToWeek = new Intent(this, WeekView.class);
+                goToWeek.putExtra(DATA, array);
+                startActivity(goToWeek);
+                break;
+            }
+            case(R.id.monthButton):
+            {
+                Intent goToMonth = new Intent(this, MonthView.class);
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.yearButton):
+            {
+                Intent goToYear = new Intent(this, YearView.class);
+                startActivity(goToYear);
+                break;
+            }
+            case(R.id.addDetailsButton):
+            {
+                Intent goToDetails = new Intent(this, AddDetails.class);
+                goToDetails.putExtra(DATA, array);
+                startActivity(goToDetails);
+                break;
+            }
+        }
+    }
+    public void fillDate()  //Pay attention to first and last weeks
+    {
+        TextView t = (TextView) findViewById(R.id.date);
+        if(array[7] == 1 && array[0] > 7)
+        {
+            t.setText(getMonth(array[1]-1) + array[0] + ", " + array[2]);
+        }
+        else if(array[7] > 4 && array[0] < 23)
+        {
+            t.setText(getMonth(array[1]+1) + array[0] + ", " + array[2]);
+        }
+        else
+        {
+            t.setText(getMonth(array[1]) + array[0] + ", " + array[2]);
+        }
+    }
+    public String getMonth(int m)
+    {
+        if(m == 8)
+        {
+            return("August ");
+        }
+        else if(m == 9)
+        {
+            return("September ");
+        }
+        else if(m == 10)
+        {
+            return("October ");
+        }
+        else if(m == 11)
+        {
+            return("November ");
+        }
+        else if(m == 12)
+        {
+            return("December ");
+        }
+        else if(m == 1)
+        {
+            return("January ");
+        }
+        else if(m == 2)
+        {
+            return("February ");
+        }
+        else if(m == 3)
+        {
+            return("March ");
+        }
+        else if(m == 4)
+        {
+            return("April ");
+        }
+        else if(m == 5)
+        {
+            return("May ");
+        }
+        else
+        {
+            return("");
+        }
+    }
+}
---
+++ app/src/main/java/eecs448_first_team/calender_app/YearView.java
@@ -0,0 +1,178 @@
+package eecs448_first_team.calender_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class YearView extends AppCompatActivity implements View.OnClickListener {
+    public final static String DATA = "";
+    public int[] array = new int[8];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_year_view);
+
+        findViewById(R.id.januaryButton).setOnClickListener(this);
+        findViewById(R.id.februaryButton).setOnClickListener(this);
+        findViewById(R.id.marchButton).setOnClickListener(this);
+        findViewById(R.id.aprilButton).setOnClickListener(this);
+        findViewById(R.id.mayButton).setOnClickListener(this);
+        findViewById(R.id.augustButton).setOnClickListener(this);
+        findViewById(R.id.septemberButton).setOnClickListener(this);
+        findViewById(R.id.octoberButton).setOnClickListener(this);
+        findViewById(R.id.novemberButton).setOnClickListener(this);
+        findViewById(R.id.decemberButton).setOnClickListener(this);
+
+        Intent getToYear = getIntent();
+    }
+    @Override
+    public void onClick(View view)
+    {
+        //Int array pass around [day, month, year, day of 1st, Sunday's day for WeekView, # days in month, # days in previous month, week #]
+        Intent goToMonth = new Intent(this, MonthView.class);
+        switch(view.getId())
+        {
+            case(R.id.augustButton):
+            {
+                array[0]=0;
+                array[1]=8;
+                array[2]=2016;
+                array[3]=2;
+                array[4]=0;
+                array[5]=31;
+                array[6]=0;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.septemberButton):
+            {
+                array[0]=0;
+                array[1]=9;
+                array[2]=2016;
+                array[3]=5;
+                array[4]=0;
+                array[5]=30;
+                array[6]=31;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.octoberButton):
+            {
+                array[0]=0;
+                array[1]=10;
+                array[2]=2016;
+                array[3]=7;
+                array[4]=0;
+                array[5]=31;
+                array[6]=30;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.novemberButton):
+            {
+                array[0]=0;
+                array[1]=11;
+                array[2]=2016;
+                array[3]=3;
+                array[4]=0;
+                array[5]=30;
+                array[6]=31;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.decemberButton):
+            {
+                array[0]=0;
+                array[1]=12;
+                array[2]=2016;
+                array[3]=5;
+                array[4]=0;
+                array[5]=31;
+                array[6]=30;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.januaryButton):
+            {
+                array[0]=0;
+                array[1]=1;
+                array[2]=2017;
+                array[3]=1;
+                array[4]=0;
+                array[5]=31;
+                array[6]=31;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.februaryButton):
+            {
+                array[0]=0;
+                array[1]=2;
+                array[2]=2017;
+                array[3]=4;
+                array[4]=0;
+                array[5]=28;
+                array[6]=31;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.marchButton):
+            {
+                array[0]=0;
+                array[1]=3;
+                array[2]=2017;
+                array[3]=4;
+                array[4]=0;
+                array[5]=31;
+                array[6]=28;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.aprilButton):
+            {
+                array[0]=0;
+                array[1]=4;
+                array[2]=2017;
+                array[3]=7;
+                array[4]=0;
+                array[5]=30;
+                array[6]=31;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+            case(R.id.mayButton):
+            {
+                array[0]=0;
+                array[1]=5;
+                array[2]=2017;
+                array[3]=2;
+                array[4]=0;
+                array[5]=31;
+                array[6]=30;
+                array[7]=0;
+                goToMonth.putExtra(DATA, array);
+                startActivity(goToMonth);
+                break;
+            }
+        }
+    }
+}
