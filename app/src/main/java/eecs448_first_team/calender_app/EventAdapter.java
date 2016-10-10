d70e367fbab454166ee231e31eabe2284fe28ef7
Matthew Bauer <mjbauer95@gmail.com>
2016-10-10 17:27:24 -0500

    Add check for conflicting event.

--- app/src/main/java/eecs448_first_team/calender_app/EventAdapter.java
+++ app/src/main/java/eecs448_first_team/calender_app/EventAdapter.java
@@ -1,6 +1,7 @@
 package eecs448_first_team.calender_app;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,8 @@ import java.util.List;
 /**
  * Created by mbauer on 10/9/16.
  * based off of guide available here https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+ * EventAdapter creates a view for each calendar event that is provided.\
+ * This is used by day view to display events.
  */
 
 public class EventAdapter extends ArrayAdapter<CalendarEvent> {
@@ -22,6 +25,39 @@ public class EventAdapter extends ArrayAdapter<CalendarEvent> {
         super(context, 0, events);
     }
 
+    /**
+     * Does the event conflict with other events in the adapter.
+     * THat is, does the start time and end time of this event overlap with the start time or
+     * end time of one of the events in the adapter.
+     * @param event1 The event to use
+     * @return True if the event conflicts with another event in the list of event sprovided
+     */
+    public boolean doesConflict(CalendarEvent event1) {
+        Long startTime = event1.getStartDate();
+        Long endTime = event1.getEndDate();
+
+        for (int i = 0; i < getCount(); i++) {
+            CalendarEvent event2 = getItem(i);
+            if (event2.getID() == event1.getID()) {
+                continue;
+            }
+
+            // check for conflicting events
+            if (event2.getStartDate() < event1.getEndDate() && event2.getEndDate() > event1.getStartDate()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the view for each event.
+     * @param position Position for each element in the view
+     * @param convertView The view provided for each laout
+     * @param parent The parent element of the view
+     * @return An initialized view object
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
@@ -61,6 +97,11 @@ public class EventAdapter extends ArrayAdapter<CalendarEvent> {
 
         TextView endTime = (TextView) convertView.findViewById(R.id.endTime);
         endTime.setText(dateFormat.format(event.getEndDate()));
+
+        if (doesConflict(event)) {
+            startTime.setTextColor(Color.RED);
+            endTime.setTextColor(Color.RED);
+        }
 
         // Return the completed view to render on screen
         return convertView;
